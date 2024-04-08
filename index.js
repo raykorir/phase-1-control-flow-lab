3fb48e0c289e7d499cd9cd322ffd0890b5cb0b12
raymond <raymond.korir@student.moringaschool.com>
2024-04-09 00:01:41 +0300

    flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting;
+  if (feet <= 400) {
+    greeting = 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    greeting = 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else {
+    greeting = 'No can do.';
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch(tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+  }
+  return response;
 }
